53e0c8513646b905f7ccb0804020120f1dcf1e1f
sakutomo07 <arai15735320@gmail.com>
2020-06-11 07:30:43 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,28 +12,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password: 'foo',
                                          password_confirmation: 'bar' } }
     end
-
-    # 成功時のテスト
-    test 'valid signup information' do
-      get signup_path
-      assert_difference 'User.count', 1 do
-        post users_path, params: { user: { name: 'Example User',
-                                           email: 'user@example.com',
-                                           password: 'password',
-                                           password_confirmation: 'password' } }
-      end
-      # 指定されたリダイレクト先に移動するメソッド
-      follow_redirect!
-      # 'users/show'のテンプレート表示テスト
-      assert_template 'users/show'
-      # flashのテスト
-      assert_not flash.empty?
-    end
-
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
     assert_select 'form[action="/signup"]'
+  end
+
+  # 成功時のテスト
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'password',
+                                         password_confirmation: 'password' } }
+    end
+    # 指定されたリダイレクト先に移動するメソッド
+    follow_redirect!
+    # 'users/show'のテンプレート表示テスト
+    assert_template 'users/show'
+    # flashのテスト
+    assert_not flash.empty?
   end
   # test "the truth" do
   #   assert true
